b88c2d78307ead8aa09d3c71a29d7287c54465b1
Vandana Chandrakar <70029083+vandana1519@users.noreply.github.com>
2021-02-15 12:05:25 +0530

    Add files via upload

---
+++ src/main/java/com/hackathon/swipemanagement/SwipeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.swipemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/swipemanagement/SwipeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.swipemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
